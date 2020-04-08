aa1917b5d2b22bcc9431d099fe46d7cf243561ba
ilakshani <lakshithaviraj36@gmail.com>
2020-04-08 20:47:53 +0530

    new

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const interval = setInterval(function() {
 }, 300000);
 
 app.listen(port, () => {
-  attendance.resetAtMidNight();
   console.log(`Server running at port ` + port);
   console.log(Date().toString());
   previousDate = new Date().getDate();
